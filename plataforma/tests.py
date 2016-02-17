e801ab78e096c133733e807d9f8ecd5c987be79c
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2016-02-16 22:42:41 -0500

    función to_python_object reemplazada por un mock

--- plataforma/tests.py
+++ plataforma/tests.py
@@ -2,16 +2,38 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from plataforma.models import *
-from plataforma.similarity import *
+
+from mock import Mock
 
 
 
-class SimilarityTest(TestCase):
+# class ToPythonObjectTest(TestCase):
+# 	def test_empty_questionnaire(self):
+# 		questionnaire=[]
+# 		self.assertEqual(to_python_object(questionnaire),'{}')
+# 	def test_malformed_questionnaire(self):	
+# 		questionnaire=[{1:"pregunta"}]
+# 		self.assertRaises(Exception,to_python_object,questionnaire)
+
+
+from plataforma import similarity
+similarity.to_python_object = Mock(return_value='{111}')
+from plataforma.similarity import cuestionario_afinidad
+
+
+class CuestionarioAfinidadTest(TestCase):	
 	def test_empty_questionnaire(self):
-		questionnaire=[]
-		self.assertEqual(to_python_object(questionnaire),'{}')
+		questionnaires=[]
+		problemas_soluciones=[]
+		pagina=1
+		num_registros=10
+		expected_answer = {'problemas_soluciones':[], 'total':0}
+		self.assertEqual(cuestionario_afinidad(questionnaires,problemas_soluciones,pagina,num_registros),expected_answer)
 	def test_malformed_questionnaire(self):	
-		questionnaire=[{1:"pregunta"}]
-		self.assertRaises(Exception,to_python_object,questionnaire)
-		
+		questionnaires=[{1:"pregunta"}]
+		problemas_soluciones=[]
+		pagina=1
+		num_registros=10
+		expected_answer = {'problemas_soluciones':[], 'total':0}
+		self.assertEqual(cuestionario_afinidad(questionnaires,problemas_soluciones,pagina,num_registros),expected_answer)
 
