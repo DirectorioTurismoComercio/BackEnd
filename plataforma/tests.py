23a00194c17aaf3828ee3c9028aea1290ad7bd3a
Camilo Rodríguez <camilo255423@hotmail.com>
2016-02-29 12:34:14 -0500

    Prueba unitaria creación de usuario correctamente

--- plataforma/tests.py
+++ plataforma/tests.py
@@ -3,6 +3,8 @@ from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from plataforma.models import *
 from plataforma.similarity import *
+from rest_framework.test import APITestCase
+
 
 
 
@@ -14,4 +16,27 @@ class SimilarityTest(TestCase):
 		questionnaire=[{1:"pregunta"}]
 		self.assertRaises(Exception,to_python_object,questionnaire)
 		
+class CreateUserTest(APITestCase):
+    
+    def setUp(self):
+    	Rol.objects.create(id=2,nombre='Comerciante')
+
+	def test_succesful_created(TestCase):
+		url = '/usuarios/'
+		data = {"nombres": "Alejandro", 
+        "apellido1": "Latorre", 
+        "apellido2": "Latorre", 
+        "numero_documento": null, 
+        "correo": "alatorreaxcasdasaasdasdasdasdasdasdasdasdasdasd16@aol.com", 
+        "nombre_institucion": "Programador .JS", 
+        "telefono_institucion": "9999999", 
+        "ubicacion_institucion": "Subachoque", 
+        "direccion_institucion": "Calle 24", 
+        "correo_institucion": null, 
+        "municipio_id": 96, 
+        "rol": 2
+        } 
+        response = self.client.post(url,data,format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
