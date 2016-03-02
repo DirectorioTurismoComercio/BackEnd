d6acf7b75a69525920b36a7460fc1c7f8212560a
AndresDiaz1 <desarrollo2@iMac-de-Vivelab.local>
2016-03-02 17:07:41 -0500

    solución bug cambio en los settings

--- authentication_module/models.py
+++ authentication_module/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import UserManager
 
 
 class CustomUser(AbstractBaseUser):
+	username = models.CharField(max_length=30, blank=True)
 	email = models.EmailField(unique=True)
 	USERNAME_FIELD = 'email'
 	objects = UserManager()
