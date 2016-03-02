242677d11637b3579c81b5d2c6992a1a8d6c2c38
Camilo Rodríguez <rodriguez.camilo@gmail.com>
2016-03-01 21:57:39 -0500

    se crearon pruebas para el proceso de autenticación

--- authentication_module/tests.py
+++ authentication_module/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from authentication_module.serializers import CustomUserSerializer
+from rest_framework import status
 
-# Create your tests here.
+
+class AuthenticationTest(TestCase):
+	WRONG_PASSWORD='12346'
+	PASSWORD='12345'
+	EMAIL='correo@correo.com'
+
+	def setUp(self):
+		user = CustomUserSerializer(data={'email':self.EMAIL,'password': self.PASSWORD})
+		user.is_valid()
+		user = user.save()
+		user.set_password(self.PASSWORD)
+		user.save()
+
+	def test_succesfully_authenticated(self):
+		data={'email':self.EMAIL,'password': self.PASSWORD}
+		response = self.client.post('/rest-auth/login',data,format='json')        
+		self.assertEqual(response.status_code, status.HTTP_200_OK, response)
+
+	def test_failed_authentication(self):
+		data={'email':self.EMAIL,'password': self.WRONG_PASSWORD}
+		response = self.client.post('/rest-auth/login',data,format='json')        
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response)
+		
