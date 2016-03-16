f78858d4abd6eaa81b07b58028f03899ccaebfd6
AndresDiaz1 <desarrollo2@iMac-de-Vivelab.local>
2016-03-16 17:46:04 -0500

    autenticación en FB google y normal
    Falta logout de FB y google para eliminar el token

--- authentication_module/views.py
+++ authentication_module/views.py
@@ -5,11 +5,28 @@ from rest_framework.authentication import TokenAuthentication
 from rest_social_auth.views import BaseSocialAuthView 
 from rest_social_auth.serializers import (OAuth2InputSerializer, OAuth1InputSerializer, UserSerializer,
     TokenSerializer, UserTokenSerializer, JWTSerializer, UserJWTSerializer)
+from plataforma.serializers import *
 
 
 class CustomSocialTokenUserAuthView(BaseSocialAuthView):
     serializer_class = UserTokenSerializer
     authentication_classes = (TokenAuthentication, )
     def do_login(self, backend, user):
-       print("login...", user.username,"////",user.email, "***", self, "||", backend)
+    	data={
+    		"nombres":user.first_name,
+    		"apellido1":user.last_name,
+    		"correo":user.email
+    	}
+     	usuario = UsuarioSerializer(data=data)
+     	#.nombre=user.first_name
+     	#usuario.apellido1=user.last_name
+     	#usuario.correo=user.email
+        usuario.user = user
+      	if usuario.is_valid():
+      		print ("////guardando")
+        	usuario.save()      
+        else:
+        	print("////////", usuario.errors)   
+        print("El usuario /////", usuario)
+       	print("login...", user.username,"////",user.email, "***", self, "||", backend)
 
