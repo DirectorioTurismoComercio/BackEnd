48769ec0146a4a567d6aca484f9ad37a5759ff9b
Ingeniero 1 <vivelabcrear@gmail.com>
2016-05-02 17:22:43 -0500

    multiple foto

--- sitios/models.py
+++ sitios/models.py
@@ -9,8 +9,8 @@ class Sitio(models.Model):
 	latitud = models.DecimalField(max_digits=20, decimal_places=18)
 	longitud = models.DecimalField(max_digits=20, decimal_places=18)
 	descripcion = models.TextField(null=True)
+
+class Foto(models.Model):
 	URLfoto = models.FileField(upload_to='Fotos/')
-
-
-
+	sitio=models.ForeignKey(Sitio, related_name='fotos')
 
--- sitios/serializers.py
+++ sitios/serializers.py
@@ -4,4 +4,6 @@ from sitios.models import Sitio
 class SitioSerializer(serializers.ModelSerializer):  
         class Meta:
             model = Sitio
+            fields = ('nombre','latitud','longitud','descripcion','fotos')
+
 
