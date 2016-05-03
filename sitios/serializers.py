c7d850428247de54b9215048c20687c7b2e092c0
Ingeniero 1 <vivelabcrear@gmail.com>
2016-05-03 09:39:46 -0500

    serializer de prueba para multifoto

--- sitios/serializers.py
+++ sitios/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from sitios.models import Sitio
+from sitios.models import Foto 
+
+class FotoSerializer(serializers.ModelSerializer):  
+		class Meta:
+			model = Foto
 
 class SitioSerializer(serializers.ModelSerializer):  
-        class Meta:
-            model = Sitio
-            fields = ('nombre','latitud','longitud','descripcion','fotos')
+		fotos=FotoSerializer(many=True)
+		class Meta:
+			model = Sitio
+			#fields = ('nombre','latitud','longitud','descripcion')
 
+
+            
 
