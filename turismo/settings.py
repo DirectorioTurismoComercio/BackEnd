7eda7f9ae69ad7dd6edc519aeba63c21b1753773
Ingeniero0 <vivelabcrear@gmail.com>
2016-08-04 16:20:42 -0500

    al actualizar los datos y entrar por red social los datos se mantienen

--- turismo/settings.py
+++ turismo/settings.py
@@ -93,7 +93,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.user.create_user',
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details',
     'users.social_pipeline.save_avatar',  # custom action
 )
 
