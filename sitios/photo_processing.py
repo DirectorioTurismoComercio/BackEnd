e5eab22dd9e7212dd789f983be38fc42d3628cbd
Ingeniero0 <vivelabcrear@gmail.com>
2016-09-02 15:26:09 -0500

    se reescala y reduce tamaño de las fotos. tamaño max 1200x900

--- sitios/photo_processing.py
+++ sitios/photo_processing.py
@@ -3,7 +3,7 @@ import os
 import imghdr
 from django.conf import settings
 
-def reduce_photo_size(photo_path, photo_filename):
+def reduce_photo_size(photo_path, photo_filename,size):
 	quality = settings.PORCENTAJE_CALIDAD_FOTOS
 	max_width = settings.MAXIMO_ANCHO_FOTOS;
 	max_height = settings.MAXIMO_ALTO_FOTOS;
@@ -19,27 +19,28 @@ def reduce_photo_size(photo_path, photo_filename):
 		new_width = int(round(file.size[0]/scale_factor))
 		new_height = int(round(file.size[1]/scale_factor))
 		file = file.resize((new_width,new_height),Image.ANTIALIAS)
+		file.save(photo_path+"/"+photo_filename,'JPEG',optimize=True,quality=100)
+	if size > settings.MAX_TAMANO_IMAGEN_SIN_REDUCCION:
+		if imghdr.what(photo_path+"/"+photo_filename)=='png' or imghdr.what(photo_path+"/"+photo_filename).lower()=='gif':
+			photo_filename = os.path.splitext(photo_filename)[0]+'.jpg'
+			file.load() 
+			background = Image.new("RGB", file.size, (255, 255, 255))
+			background.paste(file, mask=file.split()[3]) 
+			background.save(photo_path+"/"+photo_filename,'JPEG',optimize=True,quality=quality)
 
-	if imghdr.what(photo_path+"/"+photo_filename)=='png' or imghdr.what(photo_path+"/"+photo_filename).lower()=='gif':
-		photo_filename = os.path.splitext(photo_filename)[0]+'.jpg'
-		file.load() 
-		background = Image.new("RGB", file.size, (255, 255, 255))
-		background.paste(file, mask=file.split()[3]) 
-		background.save(photo_path+"/"+photo_filename,'JPEG',optimize=True,quality=quality)
-
-	if imghdr.what(photo_path+"/"+photo_filename).lower()=='jpg' or imghdr.what(photo_path+"/"+photo_filename).lower()=='jpeg':
-		file.save(photo_path+"/"+photo_filename,'JPEG',optimize=True,quality=quality)
+		if imghdr.what(photo_path+"/"+photo_filename).lower()=='jpg' or imghdr.what(photo_path+"/"+photo_filename).lower()=='jpeg':
+			file.save(photo_path+"/"+photo_filename,'JPEG',optimize=True,quality=quality)
+	
 
 	return photo_filename
 
 
 def reduce_site_photos(dir,site_photos):
 	for photo in site_photos:
-		old_name = photo.URLfoto.name
-		if photo.URLfoto.size > settings.MAX_TAMANO_IMAGEN_SIN_REDUCCION:
-			photo.URLfoto.name = reduce_photo_size(dir,photo.URLfoto.name)
-			if old_name != photo.URLfoto.name:
-				os.remove(dir+"/"+old_name)
+		old_name = photo.URLfoto.name	
+		photo.URLfoto.name = reduce_photo_size(dir,photo.URLfoto.name,photo.URLfoto.size)
+		if old_name != photo.URLfoto.name:
+			os.remove(dir+"/"+old_name)
 		photo.URLfoto.name = os.path.splitext(photo.URLfoto.name)[0]+'.jpg'
 		photo.save()
 		if os.path.isfile(dir+"/"+old_name):
