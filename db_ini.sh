484baf136b234c7086a7c6d46d6c3130a8203a68
Ingeniero1 <vivelabcrear@gmail.com>
2016-09-22 16:00:01 -0500

    Se agrega utf8 al db_ini

--- db_ini.sh
+++ db_ini.sh
@@ -1,4 +1,4 @@
 mysql -u root -proot -e 'drop database plataforma;create database plataforma  CHARACTER SET latin1 COLLATE latin1_swedish_ci'
 
 python manage.py migrate
-mysql -u root -proot plataforma < datos_iniciales.sql
+mysql -u root -proot --default-character-set=utf8  plataforma < datos_iniciales.sql
