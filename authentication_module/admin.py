1605bd5874daae6dd8212cb430e7f7b4ec3b118f
Ingeniero0 <vivelabcrear@gmail.com>
2016-10-03 14:51:49 -0500

    se muestra si es cuenta activa o no en el administrador

--- authentication_module/admin.py
+++ authentication_module/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import CustomUser
-# Register your models here.
-admin.site.register(CustomUser)
+
+
+
+class UserAdmin(admin.ModelAdmin):
+	ordering = ('email', )
+	list_display = ('email', 'es_cuenta_activa')
+
+
+admin.site.register(CustomUser, UserAdmin)
