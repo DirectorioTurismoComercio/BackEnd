00513d02ed140a68169cc9540fed01d9c2b37dc6
Ingeniero 1 <vivelabcrear@gmail.com>
2016-10-27 10:40:35 -0500

    Se coloca una excepción para el manejo de errores en la traducción del sitio

--- translator/translator.py
+++ translator/translator.py
@@ -5,7 +5,10 @@ class Translator():
     def getTranslatedWord(self,keyword):
         translatorAPIKey="trnsl.1.1.20161024T195702Z.b306b5c031a2ac5e.12c39e368a8b60f0c7d74ed76e03b3699c2fa1a1"
         keyword=keyword.encode('utf-8')
-        translateResponse = urllib2.urlopen("https://translate.yandex.net/api/v1.5/tr.json/translate?key="+translatorAPIKey+"&text="+keyword+"&lang=es-en").read()
-        JSONTranslateResponse = json.loads(translateResponse)
-        translatedKeyword= JSONTranslateResponse['text'][0]
+        try:
+            translateResponse = urllib2.urlopen("https://translate.yandex.net/api/v1.5/tr.json/translate?key="+translatorAPIKey+"&text="+keyword+"&lang=es-en", timeout=5).read()
+            JSONTranslateResponse = json.loads(translateResponse)
+            translatedKeyword = JSONTranslateResponse['text'][0]
+        except urllib2.URLError, e:
+            translatedKeyword = ''
         return translatedKeyword
